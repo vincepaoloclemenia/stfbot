f1d4f781a4fdab46e3f6e62d9e7e0891f927413b
Vincent Paolo <vincentpaoloclemenia@gmail.com>
2017-12-06 09:02:23 +0800

    fixed sidebar and nav for each users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  belongs_to :company
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :first_name, :last_name, :role
